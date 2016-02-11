72ea06289d3fcaef3dc8c16047b0bc32768a6955
Dmitry Rumiantsev <dr270797@gmail.com>
2016-02-11 21:18:53 +0300

    Update Message.java, add Main.java

---
+++ CleanCode/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by User on 11.02.2016.
+ */
+public class Main {
+    public static void main(String args[])
+    {
+        String id="f4512e252d";
+        String author="Dmitry";
+        String message="This statement is false";
+        Message m1=new Message(id,message,author);
+        System.out.println(m1);
+    }
+}
--- CleanCode/src/Message.java
+++ CleanCode/src/Message.java
@@ -1,6 +1,27 @@
 /**
  * Created by User on 11.02.2016.
  */
-public class Message {
-    
+import java.sql.Timestamp;
+class Message {
+    String id;
+    String message;
+    String author;
+    Timestamp timestamp;
+    public Message(String id,String message,String author)
+    {
+        this.id=id;
+        this.message=message;
+        this.author=author;
+        this.timestamp=new Timestamp(System.currentTimeMillis());
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id='" + id + '\'' +
+                ", message='" + message + '\'' +
+                ", author='" + author + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
