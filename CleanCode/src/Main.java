9bb716794058ad0dda1d323d30501a8822d7bbcb
Dmitry Rumiantsev <dr270797@gmail.com>
2016-02-22 21:55:35 +0300

    Made fields in Message.java private, added proper intendation, cleaned code

--- CleanCode/src/Main.java
+++ CleanCode/src/Main.java
@@ -8,7 +8,8 @@ import java.util.regex.*;
 import java.util.logging.*;
 
  class Main {
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     private static ArrayList<String> warnings;
     private static StringBuffer query;
     private static ArrayList<Message> history;
@@ -21,6 +22,7 @@ import java.util.logging.*;
                 warnings.add("Author name is too long");
                 System.out.println("Warning: author name is too long");
             }
+
             System.out.println("Input message");
             String message = reader.readLine();
             if(message.length()>140) {
@@ -28,18 +30,22 @@ import java.util.logging.*;
                 System.out.println("Warning: your message is too long");
             }
             query.append(", Author: ").append(author).append(", Message: ").append(message).append(". Added 1 message.");
+
             history.add(new Message(message, author));
-            logger.log(Level.INFO,query.toString());
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+
+            LOGGER.log(Level.INFO,query.toString());
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
         catch (IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
     }
 
@@ -49,18 +55,21 @@ import java.util.logging.*;
             System.out.println("Input id");
             String id = reader.readLine();
             query.append(", ID: ").append(id);
+
             for (int i = 0; i < history.size(); i++) {
-                if (history.get(i).id.toString().equals(id))
+                if (history.get(i).getId().toString().equals(id)) {
                     history.remove(i);
+                }
             }
+
             query.append(". Removed 1 message");
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
         }
         catch (IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
         }
     }
      private static void searchByAuthor(BufferedReader reader)
@@ -69,20 +78,23 @@ import java.util.logging.*;
             System.out.println("Input author");
             String author = reader.readLine();
             query.append(", Author: ").append(author);
+
             int messagesFound=0;
-            for (Message message : history)
-                if (message.author.equals(author)) {
+            for (Message message : history) {
+                if (message.getAuthor().equals(author)) {
                     System.out.print(message + "\n");
                     messagesFound++;
                 }
+            }
+
             query.append(". Found ").append(messagesFound).append(" messages");
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
         }
         catch (IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
         }
     }
      private static void searchByLexeme(BufferedReader reader)
@@ -91,20 +103,23 @@ import java.util.logging.*;
             System.out.println("Input lexeme");
             String lexeme = reader.readLine();
             query.append(", Lexeme: ").append(lexeme);
+
             int messagesFound=0;
-            for (Message message : history)
+            for (Message message : history) {
                 if (message.toString().contains(lexeme)) {
                     System.out.print(message + "\n");
                     messagesFound++;
                 }
+            }
+
             query.append(". Found ").append(messagesFound).append(" messages.");
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
         }
         catch (IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
         }
     }
      private static void searchByRegex(BufferedReader reader)
@@ -114,6 +129,7 @@ import java.util.logging.*;
             String patternString = reader.readLine();
             Pattern pattern = Pattern.compile(patternString);
             query.append(", Pattern: ").append(patternString);
+
             int messagesFound=0;
             for (Message message : history) {
                 Matcher matcher = pattern.matcher(message.toString());
@@ -122,49 +138,59 @@ import java.util.logging.*;
                     messagesFound++;
                 }
             }
+
             query.append(". Found ").append(messagesFound).append(" messages.");
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
         }
         catch(IllegalArgumentException|IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
         }
 
     }
      private static void storeHistory()
     {
         try (Writer writer = new PrintWriter(new FileOutputStream(new File("history.json") )) ) {
+
             Gson gson = new GsonBuilder().create();
+
             if(history.size()==0)
             {
                 warnings.add("Saving empty history will delete the current contents of the file");
                 System.out.println("Warning: Saving empty history will delete the current contents of the file (you're not given a choice)");
             }
             gson.toJson(history,writer);
-            logger.log(Level.INFO,query.toString());
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+
+            LOGGER.log(Level.INFO,query.toString());
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
         catch(JsonIOException|IOException ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
     }
      private static void loadHistory()
     {
         try (Reader reader = new FileReader(new File("history.json"))){
-                Gson gson = new GsonBuilder().create();
-                Message[] arr1;
-                arr1= gson.fromJson(reader, Message[].class);
-                query.append(". Removed ").append(history.size()).append(" messages");
-                history.clear();
+
+            Gson gson = new GsonBuilder().create();
+            Message[] arr1;
+            arr1= gson.fromJson(reader, Message[].class);
+
+            query.append(". Removed ").append(history.size()).append(" messages");
+
+            history.clear();
             Collections.addAll(history, arr1);
+
                 for(int i=0;i<history.size();i++) {
                     for (int j = 0; j < history.size(); j++) {
                         if (history.get(i).equals(history.get(j)) && i != j) {
@@ -174,32 +200,38 @@ import java.util.logging.*;
                         }
                     }
                 }
+
             query.append(". Added ").append(history.size()).append(" messages.");
-            logger.log(Level.INFO,query.toString());
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+            LOGGER.log(Level.INFO,query.toString());
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
         catch(Exception ex)
         {
-            logger.log(Level.INFO,query.toString());
+            LOGGER.log(Level.INFO,query.toString());
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
-            for(String warning:warnings)
-                logger.log(Level.WARNING,warning);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
+            for(String warning:warnings) {
+                LOGGER.log(Level.WARNING, warning);
+            }
         }
     }
      private static void viewHistory()
     {
-        if(history.size()==0)
+        if(history.size()==0) {
             System.out.println("History is empty");
+        }
         else {
             history.sort(new Message("Hello world", "John Bull"));
-            for (Message message : history)
+            for (Message message : history) {
                 System.out.println(message + "\n");
+            }
         }
-        logger.log(Level.INFO,query.toString()+". Viewed "+history.size()+" messages.");
+
+        LOGGER.log(Level.INFO,query.toString()+". Viewed "+history.size()+" messages.");
     }
-     private static void makeStringForTimestamp(StringBuffer buffer)
+     private static void prepareStringForTimestamp(StringBuffer buffer)
     {
         if(buffer.length()>=13 && buffer.length()<19) {
             while (buffer.length() < 19) {
@@ -215,36 +247,42 @@ import java.util.logging.*;
      private static void viewTimeRange(BufferedReader reader)
     {
         if(history.size()==0) {
+
             System.out.println("History is empty");
-            logger.log(Level.INFO, query.toString()+". Viewed 0 messages");
+            LOGGER.log(Level.INFO, query.toString()+". Viewed 0 messages");
         }
         else {
             try {
                 System.out.println("Input lower bound ( yyyy-mm-dd hh:mm:ss.[fff...] format, minutes and seconds may be omited)");
                 String lowerBound = reader.readLine();
                 StringBuffer buffer = new StringBuffer(lowerBound);
-                makeStringForTimestamp(buffer);
+                prepareStringForTimestamp(buffer);
                 Timestamp min = Timestamp.valueOf(buffer.toString());
+
                 System.out.println("Input upper bound ( yyyy-mm-dd hh:mm:ss.[fff...] format, minutes and seconds may be omited)");
                 String upperBound = reader.readLine();
                 buffer.setLength(0);
                 buffer.append(upperBound);
-                makeStringForTimestamp(buffer);
+                prepareStringForTimestamp(buffer);
                 Timestamp max = Timestamp.valueOf(buffer.toString());
+
                 query.append(", min: ").append(min).append(", max: ").append(max);
+
                 history.sort(new Message("Hello world", "John Bull"));
                 int messagesFound = 0;
                 for (Message message : history) {
-                    if (!message.timestamp.before(min) && !message.timestamp.after(max)) {
+                    if ((!message.getTimestamp().before(min)) && (!message.getTimestamp().after(max))) {
                         System.out.println(message + "\n");
                         messagesFound++;
                     }
                 }
-                logger.log(Level.INFO, query.toString() + ". Viewed " + messagesFound + " messages.");
+
+                LOGGER.log(Level.INFO, query.toString() + ". Viewed " + messagesFound + " messages.");
+
             } catch (IllegalArgumentException | IOException ex) {
-                logger.log(Level.INFO, query.toString());
+                LOGGER.log(Level.INFO, query.toString());
                 System.out.println("Exception: "+ex.getMessage());
-                logger.log(Level.SEVERE, ex.getMessage(), ex);
+                LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
             }
         }
     }
@@ -253,29 +291,39 @@ import java.util.logging.*;
         history= new ArrayList<>();
         warnings= new ArrayList<>();
         query=new StringBuffer("");
-        logger.setUseParentHandlers(false);
+
+        LOGGER.setUseParentHandlers(false);
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$-7s   %5$s %6$s%n");
+
         try {
             Handler fileHandler = new FileHandler("logfile.txt", true);
+
             PrintWriter writer = new PrintWriter("logfile.txt");
             writer.print("");
             writer.close();
+
             fileHandler.setFormatter(  new SimpleFormatter());
-            logger.addHandler(fileHandler);
-            logger.setLevel(Level.INFO);
+            LOGGER.addHandler(fileHandler);
+            LOGGER.setLevel(Level.INFO);
+
             PrintStream outPrintStream = new PrintStream(new BufferedOutputStream(new FileOutputStream("logfile.txt", true)));  // append is true
             System.setErr(outPrintStream);    // redirect System.err
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             boolean isWorking = true;
             String help = "Type 'add' to add message, 'removeById' to remove message by identifier, 'load' to load from file,\n " +
                     "'save' to save to file. Type 'view' to view history or 'viewTimeRange' to view time range. Type 'searchByAuthor',\n 'searchByLexeme' or 'searchByRegex' to " +
                     "search by author, lexeme or regular expression. Type 'help' to see this message again. Type 'exit' to exit.";
             System.out.println(help);
+
             while (isWorking) {
+
                 warnings.clear();
                 query.setLength(0);
+
                 String command = reader.readLine();
                 query.append("Query: ").append(command);
+
                 switch (command) {
                     case "add":
                         addMessage(reader);
@@ -319,7 +367,7 @@ import java.util.logging.*;
         catch (IOException ex)
         {
             System.out.println("Exception: "+ex.getMessage());
-            logger.log(Level.SEVERE,ex.getMessage(),ex);
+            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
         }
     }
 }
--- CleanCode/src/Message.java
+++ CleanCode/src/Message.java
@@ -2,18 +2,13 @@ import java.sql.Timestamp;
 import java.util.Comparator;
 import java.util.UUID;
 class Message implements Comparator<Message> {
-    final UUID id;
-    final String author;
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Message && id.equals(((Message) (obj)).id);
-    }
-    public int compare(Message message1, Message message2)
-    {
-        return message1.timestamp.compareTo(message2.timestamp);
-    }
-    final Timestamp timestamp;
+
+
+    private final UUID id;
+    private final String author;
+    private final Timestamp timestamp;
     private final String message;
+
     public Message(String message,String author)
     {
         this.id=UUID.randomUUID();
@@ -21,6 +16,30 @@ class Message implements Comparator<Message> {
         this.author=author;
         this.timestamp=new Timestamp(System.currentTimeMillis());
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Message && id.equals(((Message) (obj)).id);
+    }
+
+    public int compare(Message message1, Message message2)
+    {
+        return message1.timestamp.compareTo(message2.timestamp);
+    }
+
+
 
     @Override
     public String toString() {
