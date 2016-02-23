210977061ef0bf43645a904918eede18dd25cdba
Dmitry Rumiantsev <dr270797@gmail.com>
2016-02-23 23:48:12 +0300

    Removed all methods from Main except main. Added read parameter

--- CleanCode/src/Main.java
+++ CleanCode/src/Main.java
@@ -1,275 +1,184 @@
 
 import java.io.*;
 import java.util.ArrayList;
-import com.google.gson.*;
 import java.util.LinkedList;
 import java.util.logging.*;
 
  class Main {
 
-     private static final int MAX_FIELD_LENGTH=140;
+     private static final int MAX_FIELD_LENGTH = 140;
      private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
      private static ArrayList<String> warnings;
      private static StringBuffer query;
      private static ArrayList<Message> history;
 
-    private static void addMessage(BufferedReader reader) throws IOException
-    {
-            System.out.println("Input author");
-            String author = reader.readLine();
-            if(author.length()>MAX_FIELD_LENGTH) {
-                warnings.add("Author name is too long");
-                System.out.println("Warning: author name is too long");
-            }
+     public static void main(String args[]) {
 
-            System.out.println("Input message");
-            String message = reader.readLine();
-            if(message.length()>MAX_FIELD_LENGTH) {
-                warnings.add("Your message is too long");
-                System.out.println("Warning: your message is too long");
-            }
-            query.append(", Author: ").append(author).append(", Message: ").append(message).append(". Added 1 message.");
+         history = new ArrayList<>();
+         warnings = new ArrayList<>();
+         query = new StringBuffer("");
 
-            history.add(new Message(message, author));
+         Handler fileHandler = null;
+         PrintWriter writer = null;
+         PrintStream outPrintStream = null;
+         BufferedReader reader = null;
 
-            LOGGER.log(Level.INFO,query.toString());
-            for(String warning:warnings) {
-                LOGGER.log(Level.WARNING, warning);
-            }
-    }
+         LOGGER.setUseParentHandlers(false);
+         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$-7s   %5$s %6$s%n");
 
-    private static void removeMessageById(BufferedReader reader) throws IOException
-    {
-            System.out.println("Input id");
-            String id = reader.readLine();
-            query.append(", ID: ").append(id);
+         try {
+             fileHandler = new FileHandler("logfile.txt", true);
 
-            LinkedList<Message> messagesToRemove=ChatHistoryOperations.removeMessageById(id,history);
+             writer = new PrintWriter("logfile.txt");
+             writer.print("");
+             writer.close();
 
-            query.append(messagesToRemove.size()!=0 ? ". Removed 1 message" : "removed 0 messages");
-            LOGGER.log(Level.INFO,query.toString());
-    }
-     private static void searchByAuthor(BufferedReader reader) throws IOException
-    {
-            System.out.println("Input author");
-            String author = reader.readLine();
-            query.append(", Author: ").append(author);
+             fileHandler.setFormatter(new SimpleFormatter());
+             LOGGER.addHandler(fileHandler);
+             LOGGER.setLevel(Level.INFO);
 
-            LinkedList<Message> messagesFound=ChatHistoryOperations.searchByParameter(author,"by author",history);
+             outPrintStream = new PrintStream(new BufferedOutputStream(new FileOutputStream("logfile.txt", true)));  // append is true
+             System.setErr(outPrintStream);    // redirect System.err
 
-            query.append(". Found ").append(messagesFound.size()).append(" messages");
-            LOGGER.log(Level.INFO,query.toString());
-    }
-     private static void searchByLexeme(BufferedReader reader) throws IOException
-    {
-            System.out.println("Input lexeme");
-            String lexeme = reader.readLine();
-            query.append(", Lexeme: ").append(lexeme);
+             reader = new BufferedReader(new InputStreamReader(System.in));
+             boolean isWorking = true;
+             String help = "Type 'add' to add message, 'remove' to remove message by identifier, 'load' to load from file,\n " +
+                     "'save' to save to file. Type 'view' to view history or 'viewTimeRange' to view time range. Type 'searchByAuthor',\n 'searchByLexeme' or 'searchByRegex' to " +
+                     "search by author, lexeme or regular expression. Type 'help' to see this message again. Type 'exit' to exit.";
+             System.out.println(help);
 
-            LinkedList<Message> messagesFound=ChatHistoryOperations.searchByParameter(lexeme,"by lexeme",history);
+             while (isWorking) {
 
-            query.append(". Found ").append(messagesFound.size()).append(" messages.");
-            LOGGER.log(Level.INFO,query.toString());
-    }
-     private static void searchByRegex(BufferedReader reader)
-    {
-        try {
-            System.out.println("Input pattern");
-            String patternString = reader.readLine();
-            query.append(", Pattern: ").append(patternString);
+                 warnings.clear();
+                 query = new StringBuffer("");
 
-            LinkedList<Message> messagesFound=ChatHistoryOperations.searchByParameter(patternString,"by regex",history);
+                 String command = reader.readLine();
+                 query.append("Query: ").append(command);
+                 try {
+                     switch (command) {
+                         case "add": {
+                             history.add(new Message(readParameter(reader, "message"), readParameter(reader, "author")));
+                             break;
+                         }
+                         case "remove": {
+                             int sizeBefore = history.size();
+                             ChatHistoryOperations.removeMessageById(readParameter(reader, "id"), history);
+                             query.append(history.size() != sizeBefore ? ". Removed 1 message" : ". Removed 0 messages");
+                             break;
+                         }
+                         case "load": {
+                             Reader reader1 = new FileReader(new File("history.json"));
 
-            query.append(". Found ").append(messagesFound.size()).append(" messages.");
-            LOGGER.log(Level.INFO,query.toString());
-        }
-        catch(IllegalArgumentException|IOException ex)
-        {
-            LOGGER.log(Level.INFO,query.toString());
-            System.out.println("Exception: "+ex.getMessage());
-            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
-        }
+                             query.append(". Removed ").append(history.size()).append(" messages");
+                             warnings.addAll(ChatHistoryOperations.loadHistory(reader1, history));
+                             query.append(". Added ").append(history.size()).append(" messages.");
 
-    }
-     private static void storeHistory()
-    {
-        try (Writer writer = new PrintWriter(new FileOutputStream(new File("history.json") )) ) {
+                             reader1.close();
+                             break;
+                         }
+                         case "save": {
+                             Writer writer1 = new PrintWriter(new FileOutputStream(new File("history.json")));
+                             ChatHistoryOperations.storeHistory(writer1, history);
+                             writer1.close();
+                             break;
+                         }
+                         case "view": {
+                             ChatHistoryOperations.viewHistory(history);
+                             query.append(". Viewed " + history.size() + " messages.");
+                             break;
+                         }
+                         case "exit":
+                             isWorking = false;
+                             break;
+                         case "searchByAuthor": {
+                             String author = readParameter(reader, "author");
+                             LinkedList<Message> messagesFound = ChatHistoryOperations.searchByParameter(author, "by author", history);
+                             query.append(". Found ").append(messagesFound.size()).append(" messages");
+                             break;
+                         }
+                         case "viewTimeRange": {
+                             if (history.size() == 0) {
 
-            ChatHistoryOperations.storeHistory(writer,history);
-        }
-        catch(JsonIOException|IOException ex)
-        {
-            LOGGER.log(Level.INFO,query.toString());
-            System.out.println("Exception: "+ex.getMessage());
-            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
-        }
-    }
-     private static void loadHistory()
-    {
-        try (Reader reader = new FileReader(new File("history.json"))){
+                                 System.out.println("History is empty");
+                                 query.append(". Viewed 0 messages");
+                             } else {
+                                 String min = readParameter(reader, "lower bound");
+                                 String max = readParameter(reader, "upper bound");
+                                 LinkedList<Message> messagesFound = ChatHistoryOperations.viewTimeRange(new StringBuffer(min), new StringBuffer(max), history);
 
-            query.append(". Removed ").append(history.size()).append(" messages");
+                                 query.append(". Viewed " + messagesFound.size() + " messages.");
+                             }
+                             break;
+                         }
+                         case "searchByLexeme": {
+                             String lexeme = readParameter(reader, "lexeme");
+                             LinkedList<Message> messagesFound = ChatHistoryOperations.searchByParameter(lexeme, "by lexeme", history);
+                             query.append(". Found ").append(messagesFound.size()).append(" messages");
+                             break;
+                         }
+                         case "searchByRegex": {
+                             String pattern = readParameter(reader, "pattern");
+                             LinkedList<Message> messagesFound = ChatHistoryOperations.searchByParameter(pattern, "by regex", history);
+                             query.append(". Found ").append(messagesFound.size()).append(" messages");
+                             break;
+                         }
+                         case "help":
+                             System.out.println(help);
+                             break;
+                         default:
+                             System.out.println("Invalid command, try again");
+                             break;
+                     }
+                     LOGGER.log(Level.INFO, query.toString());
+                     for (String warning : warnings) {
+                         LOGGER.log(Level.WARNING, warning);
+                     }
+                 } catch (Exception ex) {
+                     LOGGER.log(Level.INFO, query.toString());
+                     System.out.println("Exception: " + ex.getMessage());
+                     LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+                     for (String warning : warnings) {
+                         LOGGER.log(Level.WARNING, warning);
+                     }
+                 }
+             }
+         } catch (IOException ex) {
+             LOGGER.log(Level.INFO, query.toString());
+             System.out.println("Exception: " + ex.getMessage());
+             LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+             for (String warning : warnings) {
+                 LOGGER.log(Level.WARNING, warning);
+             }
+         } finally {
+             try {
+                 if (writer != null)
+                     writer.close();
+                 if (outPrintStream != null)
+                     outPrintStream.close();
+                 if (fileHandler != null)
+                     fileHandler.close();
+                 if (reader != null)
+                     reader.close();
+             } catch (IOException ex) {
+                 System.out.println("Exception: " + ex.getMessage());
+                 LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+             }
+         }
+     }
 
-            warnings.addAll(ChatHistoryOperations.loadHistory(reader,history));
-
-            query.append(". Added ").append(history.size()).append(" messages.");
-            LOGGER.log(Level.INFO,query.toString());
-            for(String warning:warnings) {
-                LOGGER.log(Level.WARNING, warning);
-            }
-        }
-        catch(Exception ex)
-        {
-            LOGGER.log(Level.INFO,query.toString());
-            System.out.println("Exception: "+ex.getMessage());
-            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
-            for(String warning:warnings) {
-                LOGGER.log(Level.WARNING, warning);
-            }
-        }
-    }
-     private static void viewHistory()
-    {
-        ChatHistoryOperations.viewHistory(history);
-        LOGGER.log(Level.INFO,query.toString()+". Viewed "+history.size()+" messages.");
-    }
-
-     private static void viewTimeRange(BufferedReader reader)
-    {
-        if(history.size()==0) {
-
-            System.out.println("History is empty");
-            LOGGER.log(Level.INFO, query.toString()+". Viewed 0 messages");
-        }
-        else {
-            try {
-                System.out.println("Input lower bound ( yyyy-mm-dd hh:mm:ss.[fff...] format, minutes and seconds may be omited)");
-                StringBuffer lowerBound = new StringBuffer(reader.readLine());
-                query.append(", min: ").append(lowerBound.toString());
-
-                System.out.println("Input upper bound ( yyyy-mm-dd hh:mm:ss.[fff...] format, minutes and seconds may be omited)");
-                StringBuffer upperBound = new StringBuffer(reader.readLine());
-                query.append(", max: ").append(upperBound.toString());
-
-                LinkedList<Message> messagesFound=ChatHistoryOperations.viewTimeRange(lowerBound,upperBound,history);
-
-                LOGGER.log(Level.INFO, query.toString() + ". Viewed " + messagesFound.size() + " messages.");
-
-            } catch (IllegalArgumentException | IOException ex) {
-                LOGGER.log(Level.INFO, query.toString());
-                System.out.println("Exception: "+ex.getMessage());
-                LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-            }
-        }
-    }
-    public static void main(String args[])
-    {
-        history= new ArrayList<>();
-        warnings= new ArrayList<>();
-        query=new StringBuffer("");
-
-        Handler fileHandler=null;
-        PrintWriter writer=null;
-        PrintStream outPrintStream=null;
-        BufferedReader reader=null;
-
-        LOGGER.setUseParentHandlers(false);
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$-7s   %5$s %6$s%n");
-
-        try {
-            fileHandler = new FileHandler("logfile.txt", true);
-
-            writer = new PrintWriter("logfile.txt");
-            writer.print("");
-            writer.close();
-
-            fileHandler.setFormatter(  new SimpleFormatter());
-            LOGGER.addHandler(fileHandler);
-            LOGGER.setLevel(Level.INFO);
-
-            outPrintStream = new PrintStream(new BufferedOutputStream(new FileOutputStream("logfile.txt", true)));  // append is true
-            System.setErr(outPrintStream);    // redirect System.err
-
-            reader = new BufferedReader(new InputStreamReader(System.in));
-            boolean isWorking = true;
-            String help = "Type 'add' to add message, 'removeById' to remove message by identifier, 'load' to load from file,\n " +
-                    "'save' to save to file. Type 'view' to view history or 'viewTimeRange' to view time range. Type 'searchByAuthor',\n 'searchByLexeme' or 'searchByRegex' to " +
-                    "search by author, lexeme or regular expression. Type 'help' to see this message again. Type 'exit' to exit.";
-            System.out.println(help);
-
-            while (isWorking) {
-
-                warnings.clear();
-                query=new StringBuffer("");
-
-                String command = reader.readLine();
-                query.append("Query: ").append(command);
-
-                switch (command) {
-                    case "add":
-                        addMessage(reader);
-                        break;
-                    case "remove":
-                        removeMessageById(reader);
-                        break;
-                    case "load":
-                        loadHistory();
-                        break;
-                    case "save":
-                        storeHistory();
-                        break;
-                    case "view":
-                        viewHistory();
-                        break;
-                    case "exit":
-                        isWorking = false;
-                        break;
-                    case "searchByAuthor":
-                        searchByAuthor(reader);
-                        break;
-                    case "viewTimeRange":
-                        viewTimeRange(reader);
-                        break;
-                    case "searchByLexeme":
-                        searchByLexeme(reader);
-                        break;
-                    case "searchByRegex":
-                        searchByRegex(reader);
-                        break;
-                    case "help":
-                        System.out.println(help);
-                        break;
-                    default:
-                        System.out.println("Invalid command, try again");
-                        break;
-                }
-            }
-        }
-        catch (IOException ex)
-        {
-            LOGGER.log(Level.INFO,query.toString());
-            System.out.println("Exception: "+ex.getMessage());
-            LOGGER.log(Level.SEVERE,ex.getMessage(),ex);
-            for(String warning:warnings) {
-                LOGGER.log(Level.WARNING, warning);
-            }
-        }
-        finally  {
-            try {
-                if(writer!=null)
-                    writer.close();
-                if(outPrintStream!=null)
-                    outPrintStream.close();
-                if(fileHandler!=null)
-                    fileHandler.close();
-                if(reader!=null)
-                    reader.close();
-            }
-            catch (IOException ex) {
-                System.out.println("Exception: " + ex.getMessage());
-                LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
-            }
-        }
-    }
-}
+     private static String readParameter(BufferedReader reader, String name) throws IOException {
+         if (name.equals("lower bound") || name.equals("upper bound")) {
+             System.out.println("Input " + name + "( yyyy-mm-dd hh:mm:ss.[fff...] format, minutes and seconds may be omited)");
+         } else {
+             System.out.println("Input " + name);
+         }
+         String parameter = reader.readLine();
+         if (((name.equals("author")) || name.equals("message")) && (parameter.length() > MAX_FIELD_LENGTH)) {
+             warnings.add("Author name is too long");
+             System.out.println("Warning: author name is too long");
+         }
+         query.append(", " + name + ": ").append(parameter);
+         return parameter;
+     }
+ }
