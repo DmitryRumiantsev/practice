51681870dcd726a76f3be3fc91ae8a2e0f11210a
Dmitry Rumiantsev <dr270797@gmail.com>
2016-04-05 21:27:49 +0300

    Added Lab-7 project and local storage

---
+++ Lab-7/src/by/bsu/up/chat/storage/InMemoryMessageStorage.java
@@ -0,0 +1,84 @@
+package by.bsu.up.chat.storage;
+
+import by.bsu.up.chat.common.models.Message;
+import by.bsu.up.chat.logging.Logger;
+import by.bsu.up.chat.logging.impl.Log;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Collections;
+
+public class InMemoryMessageStorage implements MessageStorage {
+
+    private static final String DEFAULT_PERSISTENCE_FILE = "messages.srg";
+
+    private static final Logger logger = Log.create(InMemoryMessageStorage.class);
+
+    private ArrayList<Message> messages = new ArrayList<>();
+
+    @Override
+    public synchronized List<Message> getPortion(Portion portion) {
+        int from = portion.getFromIndex();
+        if (from < 0) {
+            throw new IllegalArgumentException(String.format("Portion from index %d can not be less then 0", from));
+        }
+        int to = portion.getToIndex();
+        if (to != -1 && to < portion.getFromIndex()) {
+            throw new IllegalArgumentException(String.format("Porting last index %d can not be less then start index %d", to, from));
+        }
+        to = Math.max(to, messages.size());
+        return messages.subList(from, to);
+    }
+
+    public InMemoryMessageStorage() {
+        loadMessages();
+    }
+
+    public void storeMessages()  {
+        try {
+            Gson gson = new GsonBuilder().create();
+            Writer writer = new PrintWriter(new FileOutputStream(new File(DEFAULT_PERSISTENCE_FILE)));
+            gson.toJson(messages, writer);
+            writer.close();
+        }
+        catch (IOException e){
+
+        }
+    }
+    public  void loadMessages()  {
+        try {
+            Reader reader = new FileReader(new File(DEFAULT_PERSISTENCE_FILE));
+            Gson gson = new GsonBuilder().create();
+            Message[] arr1;
+            arr1 = gson.fromJson(reader, Message[].class);
+            messages.clear();
+            Collections.addAll(messages, arr1);
+        }
+        catch (IOException e){
+
+        }
+    }
+    @Override
+    public void addMessage(Message message)  {
+        messages.add(message);
+        storeMessages();
+    }
+
+    @Override
+    public boolean updateMessage(Message message) {
+        throw new UnsupportedOperationException("Update for messages is not supported yet");
+    }
+
+    @Override
+    public synchronized boolean removeMessage(String messageId) {
+        throw new UnsupportedOperationException("Removing of messages is not supported yet");
+    }
+
+    @Override
+    public int size() {
+        return messages.size();
+    }
+}
